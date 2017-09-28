c67b26d87a2ad51b120d182a42da9db25aafe6cd
chashao <melvin94@gmail.com>
2017-09-27 21:25:48 -0700

    initial commit of the metadata extractor

---
+++ imagereader.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+
+def exif_extract(image_name):
+	image = Image.open(image_name)
+	return image._getexif()
+
+def non_exif_extract(image_name):
+	return image.info
+
+
+
